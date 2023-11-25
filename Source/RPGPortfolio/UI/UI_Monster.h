6df366e79417578bcf59b03b21a0c58f85e52a60
SeongJeGyeong <cmjegyeong@gmail.com>
2023-11-25 22:51:53 +0900

    UI 생성

---
+++ Source/RPGPortfolio/UI/UI_Monster.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UI_Monster.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGPORTFOLIO_API UUI_Monster : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
